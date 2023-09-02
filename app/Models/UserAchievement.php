92fe5289c93f764a81e807c4673edb3183b96830
mhdjok98 <mhdjok@gmail.com>
2023-09-02 16:37:40 +0400

    Create UserAchievement Model

---
+++ app/Models/UserAchievement.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAchievement extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'achievement_id'
+    ];
+
+    
+    /**
+     * Get the user that have the achievement.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
